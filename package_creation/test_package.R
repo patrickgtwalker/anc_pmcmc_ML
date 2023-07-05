8805b15ab3a667cf43afbecfcc35ae3209052406
jthicks <jthicks89@gmail.com>
2023-07-05 12:38:41 +0100

    To test mosquito emergence model

--- package_creation/test_package.R
+++ package_creation/test_package.R
@@ -9,21 +9,22 @@ remotes::install_github("mrc-ide/didehpc", upgrade = TRUE)
 #library(roxygen2)
 
 ## Load package from github
-devtools::install_github('jt-hicks/sifter')
+devtools::install_github('jt-hicks/sifter@issue-5',force = TRUE)
 library(sifter)
 
-test_run_sifter <- sifter::run_pmcmc(data_raw = data_sim, #I've added data_sim to the package for an easy test
-                                     n_particles = 10,
+test_run_sifter <- sifter::run_pmcmc(data_raw = sifter::data_sim, #I've added data_sim to the package for an easy test
+                                     n_particles = 200,
                                      proposal_matrix = matrix(c(0.0336,-0.000589,-0.000589,0.049420),nrow=2),
                                      max_EIR=1000,
                                      max_steps = 1e7,
-                                     atol = 1e-5,
+                                     atol = 1e-6,
                                      rtol = 1e-6,
-                                     n_steps = 3,
+                                     n_steps = 100,
                                      n_threads = 2,
                                      lag_rates = 10,
                                      country = 'Burkina Faso',
                                      admin_unit = 'Cascades',
                                      seasonality_on = 1,
                                      state_check = 0,
-                                     seasonality_check = 1)
+                                     seasonality_check = 0,
+                                     stoch_param = 'betaa')
